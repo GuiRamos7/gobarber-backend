c0512cb4eecf2359c8309a16a4745519b42e22dc
Guilherme Ramos <guilherme.ramos@matchboxbrasil.com>
2020-07-28 07:15:59 -0300

    Removing repository from services in appointments

--- src/modules/appointment/infra/http/routes/appointements.routes.ts
+++ src/modules/appointment/infra/http/routes/appointements.routes.ts
@@ -1,26 +1,27 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
-import AppointmentsRepository from '@modules/appointment/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointment/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointment/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (req, res) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
-  return res.json(appointments);
-});
+// appointmentsRouter.get('/', async (req, res) => {
+//   const appointments = await appointmentsRepository.find();
+//   return res.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (req, res) => {
   const { provider_id, date } = req.body;
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
   const appointment = await createAppointment.execute({
     provider_id,
     date: parsedDate,
